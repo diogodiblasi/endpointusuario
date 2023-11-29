c501d5cdcc79ef0fedb75c9e8af96270f7ede5de
ADS 2021.2N <36124872021.2N@senacrjedu.local>
2023-11-28 21:49:17 -0300

    second commit

--- src/main/java/com/senac/sistemacoleta/SistemaColetaApplication.java
+++ src/main/java/com/senac/sistemacoleta/SistemaColetaApplication.java
@@ -3,6 +3,10 @@ package com.senac.sistemacoleta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Prova de Nivelamento", version = "2.0", description = "Prova"))
 @SpringBootApplication
 public class SistemaColetaApplication {
 
