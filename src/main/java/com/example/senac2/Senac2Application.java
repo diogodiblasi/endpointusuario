3c9454a97c9d99fb41a5d6f27e15cbe44a3308d7
diogodiblasi <diogodiblasi@gmail.com>
2023-12-05 21:10:06 -0300

    Commit

---
+++ src/main/java/com/example/senac2/Senac2Application.java
@@ -0,0 +1,13 @@
+package com.example.senac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Senac2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Senac2Application.class, args);
+	}
+
+}
